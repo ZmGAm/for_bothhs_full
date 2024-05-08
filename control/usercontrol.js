da5e0ee5b377ac71d6cb9f997ac4aacf06597cee
ZmGAm <muneerzohaib698z.z123@gmail.com>
2024-05-08 13:05:20 +0500

    this for back end

---
+++ control/usercontrol.js
@@ -0,0 +1,63 @@
+const usermodel = require('../models/usermodel')
+const jwt = require('jsonwebtoken')
+
+module.exports.getuser = async (req, res) => {
+    const _data = await usermodel.find({})
+    if (_data) {
+        return res.send({ code: 200, message: 'success', data: _data })
+    } else {
+        return res.send({ code: 500, message: 'Service error' })
+    }
+}
+module.exports.adduser = async (req, res) => {
+
+    const { username,email, password, phone,type } = req.body
+    const userExists = await usermodel.findOne({ username: username })
+
+    
+    if (userExists) {
+        if (userExists.password === password) {
+            return res.send({ code: 400, message: ' username or Password allready taken' })
+        }
+        // const _token = await jwt.sign({ ...userExists }, 'PRIV_123')
+
+       
+    } else {
+        const _res = await usermodel.create({ username, email, password, phone,type })
+        if (_res) {
+            return res.send({ code: 300, message: 'sing up success', })
+            console.log(_res);
+        } else {
+            return res.send({ code: 500, message: 'Service error' })
+        }
+
+        // return res.send({
+        //     code: 200,
+        //     message: 'login success',
+        //     token: _token,
+        //     type: userExists.type
+        // })
+        // return res.send({ code: 500, message: 'Service error' })
+    }
+}
+
+module.exports.loginuser = async (req, res) => {
+
+    const { username, password } = req.body
+    const userExists = await usermodel.findOne({ username: username })
+    if (userExists) {
+        if (userExists.password !== password) {
+            return res.send({ code: 400, message: 'Username or Password wrong.', })
+        }
+        const _token = await jwt.sign({ ...userExists }, 'PRIV_123')
+
+        return res.send({
+            code: 200,
+            message: 'login success',
+            token: _token,
+            type: userExists.type
+        })
+    } else {
+        return res.send({ code: 500, message: 'Service error' })
+    }
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+'use server'
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cors = require('cors')
+const usercontrol = require('./control/usercontrol')
+const port = process.env.PORT || 5000; 
+const app = express()
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+mongoose.connect('mongodb://localhost:27017/car_pooling', {
+  useUnifiedTopology: true, // Keep this option
+})
+.then(() => {
+  console.log('DB Connected.');
+  // Your other code (routes, models, etc.) can go here
+})
+.catch((err) => {
+  console.error('DB Connection Error:', err);
+});
+
+
+
+  
+
+//   const userSchema = new mongoose.Schema({
+//     username: String,
+//     email: String,
+//     phone: String,
+//     password: String
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// app.post('/user', cors(), async(req, res) => {
+
+//   let user=new User();
+//   user.username= req.body.username;
+//   user.email=req.body.email;
+//   user.phone=req.body.phone;
+//   user.password=req.body.password;
+//   const doc  =await user.save();
+//   // console.log("doc",doc);
+//      res.json(doc);
+//   });
+//   app.get('/user',async (req,res)=>{
+//     const docs = await User.find({});
+//     res.json(docs)
+// })
+app.post('/user/signup', usercontrol.adduser);
+app.post('/user/login', usercontrol.loginuser);
+app.get('/user/get', usercontrol.getuser);
+
+app.listen(port, () => {
+  console.log(`Backend Running At Port ${port}`)
+})
